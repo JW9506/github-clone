8e933895faedf8107e919ce33e4777834e5e12f9
JW9506 <cf.dota@gmail.com>
2020-05-07 17:49:22 -0400

    chore: add redux support to _app

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,4 +1,21 @@
 import App from "next/app"
+import withRedux from "lib/withRedux"
 import "antd/dist/antd.css"
 
-export default App
+class MyApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {}
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx)
+      pageProps = { pageProps }
+    }
+    return pageProps
+  }
+
+  render() {
+    const { Component, pageProps, reduxStore } = this.props
+    return <Component {...pageProps} reduxStore={reduxStore} />
+  }
+}
+
+export default withRedux(MyApp)
