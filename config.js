788eeebaa75fa5c95164e73763fe814ea8976166
JW9506 <cf.dota@gmail.com>
2020-05-08 10:56:26 -0400

    chore: auth callback route

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
   github: {
+    request_token_url: "https://github.com/login/oauth/access_token",
     client_id: "Iv1.8ee2112cdbe8c790",
     client_secret: "b1a54b83bd5439595ba270b4ad92eae44779971f",
   },
