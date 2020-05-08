b928eddf396b677e175f032261eb89997325523b
JW9506 <cf.dota@gmail.com>
2020-05-08 10:52:52 -0400

    chore: add sessionStore, server session support

--- server.js
+++ server.js
@@ -1,18 +1,39 @@
 const Koa = require("koa")
+const Router = require("koa-router")
+const session = require("koa-session")
+const IORedis = require("ioredis")
 const { default: next } = require("next")
+
+const RedisSessionStore = require("./lib/sessionStore")
 
 const dev = process.env.NODE_ENV !== "production"
 const app = next({ dev })
 
 const handle = app.getRequestHandler()
 
+const redis = new IORedis()
+
 ;(async () => {
   await app.prepare()
   const server = new Koa()
+  // const router = new Router()
+
+  server.keys = ["mysessionkey1995"]
+  const SESSION_CONFIG = {
+    key: "sess",
+    maxAge: 3600 * 1000,
+    store: new RedisSessionStore(redis),
+  }
+
+  server.use(session(SESSION_CONFIG, server))
+
+  // server.use(router.routes())
+
   server.use(async (ctx, next) => {
     await handle(ctx.req, ctx.res)
     ctx.respond = false
   })
+
   server.listen(3000, () => {
     console.log("Koa waiting on 3000")
   })
