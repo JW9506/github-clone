d4eb62d56ae0ad35f70082532c1e6b21ef25858b
JW9506 <cf.dota@gmail.com>
2020-05-08 17:37:09 -0400

    chore: read client_secret in dev env

--- config.js
+++ config.js
@@ -1,7 +1,12 @@
+const { config } = require("dotenv")
+config({
+  path: ".env.development.local",
+})
+
 module.exports = {
   github: {
     request_token_url: "https://github.com/login/oauth/access_token",
     client_id: "Iv1.8ee2112cdbe8c790",
-    client_secret: "b1a54b83bd5439595ba270b4ad92eae44779971f",
+    client_secret: process.env.client_secret,
   },
 }
